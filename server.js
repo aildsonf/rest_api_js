1cb22013288c0b40e0c527f3aa0f94a8815527ba
Aildson Ferreira <ctrl.ferr@gmail.com>
2020-08-21 19:37:15 -0300

    added connection to database

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
+mongoose.connect(
+  "mongodb://ctrlferr:F4srW8Xd9mYbyHAK@cluster0-shard-00-00.9t58x.mongodb.net:27017,cluster0-shard-00-01.9t58x.mongodb.net:27017,cluster0-shard-00-02.9t58x.mongodb.net:27017/project?ssl=true&replicaSet=atlas-myg7c3-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 app.listen(5000);
