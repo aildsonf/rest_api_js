d8eb80dfc7a4370f4e4123e32aba05aa7e0bb93f
Aildson Ferreira <ctrl.ferr@gmail.com>
2020-08-21 20:41:07 -0300

    updated mongodb URI

---
+++ src/database/db_auth.js
@@ -0,0 +1,4 @@
+const db_auth =
+  "mongodb://ctrlferr:F4srW8Xd9mYbyHAK@cluster0-shard-00-00.9t58x.mongodb.net:27017,cluster0-shard-00-01.9t58x.mongodb.net:27017,cluster0-shard-00-02.9t58x.mongodb.net:27017/project?ssl=true&replicaSet=atlas-myg7c3-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+module.exports = db_auth;
