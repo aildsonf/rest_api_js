c4982cef9bec282edf613486dc645808693bf4a4
Aildson Ferreira <ctrl.ferr@gmail.com>
2020-08-22 00:16:46 -0300

    updated connection string

--- src/models/Patient.js
+++ src/models/Patient.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const PatientSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, trim: true, required: true },
   age: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
 mongoose.model("Patient", PatientSchema);
 
-// SchemaTypes available at: https://mongoosejs.com/docs/schematypes.html
+// Mongoose SchemaTypes docs: https://mongoosejs.com/docs/schematypes.html
