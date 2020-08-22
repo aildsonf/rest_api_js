04d5355333fa239946f3dcd8ea120a58519a8370
Aildson Ferreira <ctrl.ferr@gmail.com>
2020-08-22 00:18:08 -0300

    Revert "updated connection string"
    This reverts commit c4982cef9bec282edf613486dc645808693bf4a4.

--- src/models/Patient.js
+++ src/models/Patient.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const PatientSchema = new mongoose.Schema({
-  name: { type: String, trim: true, required: true },
+  name: { type: String, required: true },
   age: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
 mongoose.model("Patient", PatientSchema);
 
-// Mongoose SchemaTypes docs: https://mongoosejs.com/docs/schematypes.html
+// SchemaTypes available at: https://mongoosejs.com/docs/schematypes.html
