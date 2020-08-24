89c695e74672e44d5697ac410609595f56462826
Aildson Ferreira <ctrl.ferr@gmail.com>
2020-08-24 04:52:27 -0300

    updated schema attributes 'id_card' and 'age' to prevent setting negative values

--- src/models/Patient.js
+++ src/models/Patient.js
@@ -2,8 +2,26 @@ const mongoose = require("mongoose");
 
 const PatientSchema = new mongoose.Schema({
   name: { type: String, trim: true, required: true },
-  id_card: { type: Number, min: 0, max: 99999999999, required: true },
-  age: { type: Number, min: 0, required: true },
+
+  id_card: {
+    type: Number,
+    min: 0,
+    max: 99999999999,
+    validate(value) {
+      if (value < 0) throw new Error("invalid input");
+    },
+    required: true,
+  },
+
+  age: {
+    type: Number,
+    min: 0,
+    validate(value) {
+      if (value < 0) throw new Error("invalid input");
+    },
+    required: true,
+  },
+
   createdAt: { type: Date, default: Date.now },
 });
 
