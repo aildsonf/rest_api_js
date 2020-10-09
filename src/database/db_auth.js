736e7db9d84302be5ec431cec66579aeda843fdc
Aildson Ferreira <ctrl.ferr@gmail.com>
2020-10-09 14:46:50 -0300

    Update db_auth.js

--- src/database/db_auth.js
+++ src/database/db_auth.js
@@ -1,5 +1,5 @@
 // note: using a connection string for node.js 2.2.12 or later versions
 const db_auth =
-  "mongodb://ctrlferr:F4srW8Xd9mYbyHAK@cluster0-shard-00-00.9t58x.mongodb.net:27017,cluster0-shard-00-01.9t58x.mongodb.net:27017,cluster0-shard-00-02.9t58x.mongodb.net:27017/project?ssl=true&replicaSet=atlas-myg7c3-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb://uername:password@cluster0-shard-00-00.9t58x.mongodb.net:27017,cluster0-shard-00-01.9t58x.mongodb.net:27017,cluster0-shard-00-02.9t58x.mongodb.net:27017/project?ssl=true&replicaSet=atlas-myg7c3-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 module.exports = db_auth;
